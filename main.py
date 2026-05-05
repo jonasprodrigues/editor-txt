b36e2f0f3dfe33b12a2db166f6568ccf8300ae12
jonasprodrigues <jonas2r2@gmail.com>
2026-05-05 09:47:16 -0300

    funcao buscar_texto()

--- main.py
+++ main.py
@@ -11,10 +11,25 @@ def carregar_arquivo():
         title="Selecione arquivo:",
         filetypes=[("Arquivos de texto", "*.txt")]
     )
-    print("arquivo carregado:", caminho_arquivo)
+    label2.config(text=f"✅ Arquivo carregado: {caminho_arquivo}", fg="green")
+    #print("arquivo carregado:", caminho_arquivo)
+
 
 def buscar_texto():
-    print("buscar texto...")
+    if not caminho_arquivo:
+        label2.config(text="❌ Nenhum arquivo carregado...", fg="red")
+        return
+    
+    texto_busca = entrada.get()
+
+    with open(caminho_arquivo, "r", encoding="utf-8") as arquivo:
+        conteudo = arquivo.read()
+
+    if texto_busca in conteudo:
+        label2.config(text=f"✅ O texto '{texto_busca}' foi encontrado no arquivo!", fg="green")
+    else:
+        label2.config(text=f"❌ O texto '{texto_busca}' não foi encontrado no arquivo!", fg="red")
+
 
 root = Tk()
 root.title("Editor de Texto")
@@ -22,6 +37,9 @@ root.geometry("400x200")
 
 label = Label(root, text="Digite o texto a ser buscado:")
 label.pack(pady=10)
+
+label2 = Label(root, text="", font=("Arial", 8), fg="red")
+label2.pack(pady=10)
 
 entrada = Entry(root, width=30)
 entrada.pack(pady=5)
