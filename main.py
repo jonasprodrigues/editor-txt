4d1c064517f544ba5e15a4d686d5890db5123c65
jonasprodrigues <jonas2r2@gmail.com>
2026-05-06 09:30:16 -0300

    Log de execucao

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from tkinter import Tk, Label, Entry, Button, filedialog, messagebox
+from tkinter import Tk, Label, Entry, Button, filedialog, messagebox, Frame, Text, END
 
 print("Iniciando...")
 
 # variavel global para armazenar o caminho do arquivo
 caminho_arquivo = ""
+
+def log(mensagem):
+    caixa_texto.insert(END, mensagem + "\n")
+    caixa_texto.see(END) # rola automaticamente
 
 def carregar_arquivo():
     global caminho_arquivo
@@ -11,13 +15,14 @@ def carregar_arquivo():
         title="Selecione arquivo:",
         filetypes=[("Arquivos de texto", "*.txt")]
     )
-    label2.config(text=f"✅ Arquivo carregado: {caminho_arquivo}", fg="green")
+    #label2.config(text=f"✅ Arquivo carregado: {caminho_arquivo}", fg="green")
     #print("arquivo carregado:", caminho_arquivo)
-
+    log(f"✅ Arquivo carregado: {caminho_arquivo}")
 
 def buscar_texto():
     if not caminho_arquivo:
-        label2.config(text="❌ Nenhum arquivo carregado...", fg="red")
+        #label2.config(text="❌ Nenhum arquivo carregado...", fg="red")
+        log("❌ Nenhum arquivo carregado...")
         return
     
     texto_busca = entrada.get()
@@ -26,28 +31,46 @@ def buscar_texto():
         conteudo = arquivo.read()
 
     if texto_busca in conteudo:
-        label2.config(text=f"✅ O texto '{texto_busca}' foi encontrado no arquivo!", fg="green")
+        #label2.config(text=f"✅ O texto '{texto_busca}' foi encontrado no arquivo!", fg="green")
+        log(f"✅ O texto '{texto_busca}' foi encontrado no arquivo!")
     else:
-        label2.config(text=f"❌ O texto '{texto_busca}' não foi encontrado no arquivo!", fg="red")
+        #label2.config(text=f"❌ O texto '{texto_busca}' não foi encontrado no arquivo!", fg="red")
+        log(f"❌ O texto '{texto_busca}' não foi encontrado no arquivo!")
 
 
 root = Tk()
 root.title("Editor de Texto")
-root.geometry("400x200")
+root.geometry("600x400")
 
-label = Label(root, text="Digite o texto a ser buscado:")
+# Frames
+frame_dados = Frame(root)
+frame_dados.pack(side="left", padx=20, pady=20)
+
+frame_comandos = Frame(root)
+frame_comandos.pack(side="right", padx=20, pady=20)
+
+
+# Frame_esquerda
+label = Label(frame_dados, text="Digite o texto a ser buscado:")
 label.pack(pady=10)
 
-label2 = Label(root, text="", font=("Arial", 8), fg="red")
+label2 = Label(frame_dados, text="", font=("Arial", 8), fg="red")
 label2.pack(pady=10)
 
-entrada = Entry(root, width=30)
+entrada = Entry(frame_dados, width=30)
 entrada.pack(pady=5)
 
-botao_carregar = Button(root, text="Carregar Arquivo", command=carregar_arquivo)
+label2 = Label(frame_dados, text="Histórico de execução", font=("Arial", 8))
+label2.pack(pady=10)
+
+caixa_texto = Text(frame_dados, width=40, height=30)
+caixa_texto.pack(pady=5)
+
+# Frame_direita
+botao_carregar = Button(frame_comandos, text="Carregar Arquivo", command=carregar_arquivo)
 botao_carregar.pack(pady=5)
 
-botao_buscar = Button(root, text="Buscar Texto", command=buscar_texto)
+botao_buscar = Button(frame_comandos, text="Buscar Texto", command=buscar_texto)
 botao_buscar.pack(pady=5)
 
 root.mainloop()
